912338400c6a814238d0b47da2642a5e031cafb8
Felipe Puentes <feliipepuentes@gmail.com>
2023-02-03 08:32:00 -0500

    Creating endpoints to be used

--- ginSql/main.go
+++ ginSql/main.go
@@ -13,4 +13,30 @@ func main() {
 
 func describeEndpoints(r *gin.Engine) {
 	r.GET("/ping", ping.Ping)
+
+	///////// Employees
+	r.GET("/employees") // Get List of employees
+	r.POST("/employees")
+	r.GET("/employees/:id")
+	r.PATCH("/employees/:id")
+	r.PUT("/employees/:id")
+	r.DELETE("/employees/:id")
+
+	//////// Projects
+	r.GET("/projects")
+	r.GET("/projects/:id")
+	r.GET("/projects/task/:id")
+	r.GET("/projects/employee/:id")
+	r.POST("/projects")
+	r.PATCH("/projects/:id")
+	r.DELETE("/projects/:id")
+
+	/////// Tasks
+	r.GET("/tasks")
+	r.POST("/tasks")
+	r.GET("/tasks/:id")
+	r.GET("/tasks/employee/:id")
+	r.GET("/tasks/project/:id")
+	r.PATCH("/tasks/:id")
+	r.DELETE("/tasks/:id")
 }
